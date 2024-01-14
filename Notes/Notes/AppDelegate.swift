47adc08297a481f57f872ac53514c6ca0a221d54
niewyspana <miss.polazava@gmail.com>
2024-01-14 14:17:18 +0100

    add git ignore

---
+++ Notes/Notes/AppDelegate.swift
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.swift
+//  Notes
+//
+//  Created by Viki on 14/01/2024.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        
+        if let window = window {
+            let navigationController = UINavigationController()
+            navigationController.viewControllers = [ViewController()]
+            window.rootViewController = navigationController
+            window.makeKeyAndVisible()
+        }
+        return true
+    }
+}
+
